1aa9840c15674f1472cc408224822ba45fdf7aaf
Thomas Stoeckert <Thomas@Stoeckert.us>
2021-07-12 21:17:27 -0400

    Added filename storage

--- lib/data/file_manager.dart
+++ lib/data/file_manager.dart
@@ -4,8 +4,19 @@ import 'package:breakout_editor/data/level.dart';
 import 'package:file_selector/file_selector.dart';
 
 class FileManager {
+  static final List<XTypeGroup> _loadTypeGroups = <XTypeGroup>[
+    XTypeGroup(label: "Text Files", extensions: <String>['txt']),
+    XTypeGroup(label: "C Header Files", extensions: <String>['h']),
+    XTypeGroup(label: "Level Files", extensions: <String>['bol'])
+  ];
+  static final List<XTypeGroup> _saveTypeGroups = <XTypeGroup>[
+    XTypeGroup(label: "Text Files", extensions: <String>['txt']),
+    XTypeGroup(label: "C Header Files", extensions: <String>['h']),
+    XTypeGroup(label: "Level Files", extensions: <String>['bol'])
+  ];
+
   static Future<Level?> loadFile() async {
-    XFile? filePath = await openFile();
+    XFile? filePath = await openFile(acceptedTypeGroups: _loadTypeGroups);
 
     // Load was canceled
     if (filePath == null) return null;
@@ -13,7 +24,7 @@ class FileManager {
     // Loading data from the file, all as string
     try {
       String stringData = await filePath.readAsString();
-      return Level.fromString(stringData);
+      return Level.fromFile(stringData, filePath.name);
     } catch (e) {
       // Error during the encoding process
       throw Exception("Error reading file: $e");
@@ -22,7 +33,8 @@ class FileManager {
 
   static Future<bool> saveFile(Level data) async {
     // Get the user's requested save path
-    final String? filePath = await getSavePath();
+    final String? filePath = await getSavePath(
+        acceptedTypeGroups: _saveTypeGroups, suggestedName: data.filename);
 
     if (filePath == null) return false;
 
--- lib/data/level.dart
+++ lib/data/level.dart
@@ -2,8 +2,9 @@ import 'package:breakout_editor/data/block.dart';
 
 class Level {
   List<Block> levelData;
+  String filename;
 
-  Level(this.levelData);
+  Level(this.levelData, {this.filename = "breakout-level"});
 
   factory Level.fromString(String stringLevelData) {
     // Strip out the first and last brackets
@@ -20,12 +21,18 @@ class Level {
     return Level(parsedData);
   }
 
+  factory Level.fromFile(String stringLevelData, String filename) {
+    Level parsed = Level.fromString(stringLevelData);
+    parsed.filename = filename;
+    return parsed;
+  }
+
   factory Level.empty() {
     return Level(<Block>[]);
   }
 
   factory Level.fromLevel(Level from) {
-    return Level(from.levelData);
+    return Level(from.levelData, filename: from.filename);
   }
 
   String toCString() {
